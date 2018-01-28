00985240c1555219f94df6e337fd1cf81e3c983b
Oksana <Ksundeli76@gmail.com>
2018-01-28 19:19:41 +0300

    добавила метод contact

--- add_new_contact.py
+++ add_new_contact.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from contact import Contact
 import unittest
 
 def is_alert_present(wd):
@@ -17,47 +18,47 @@ class add_new_contact(unittest.TestCase):
     def test_add_new_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.click_on_button_add_new(wd)
-        self.create_contact(wd)
+        self.create_contact(wd, Contact(firstname="Gavrilov", middlename="Nikita", lastname="Romanovich", company="CPS",
+                       address="Tver", mobile="89301512526", byear="1986"))
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def create_contact(self, wd, firstname="Gavrilov", middlename="Nikita", lastname="Romanovich", company="CPS",
-                       address="Tver", mobile="89301512526", byear="1986"):
+    def create_contact(self, wd, contact):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(contact.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobile)
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[5]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[5]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[4]").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(byear)
+        wd.find_element_by_name("byear").send_keys(contact.byear)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def click_on_button_add_new(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd, username="admin", password="secret"):
+    def login(self, wd, username, password):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").send_keys("\\undefined")
         wd.find_element_by_name("user").click()
---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, company, address, mobile, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.byear = byear
