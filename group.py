17e9ab946844910c15db0114fab851346541607f
Oksana <Ksundeli76@gmail.com>
2018-01-28 16:42:01 +0300

    класс где лежат свойства метода из теста

---
+++ group.py
@@ -0,0 +1,6 @@
+
+class Group:
+    def __int__(self, groupname, groupheader, groupfooter):
+        self.grouoname = groupname
+        self.groupheader = groupheader
+        self.groupfooter = groupfooter
