fb2b70455b4fcdf4c3cc5c75e89b09056f474fda
Oksana <Ksundeli76@gmail.com>
2018-02-05 22:13:22 +0300

    Revert "декомпозиция"
    This reverts commit 812869dc675c41604e1d749b1e6176b8e928f721.

--- test/add_new_contact.py
+++ test/add_new_contact.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+from fixture.application_contact import Application_contact
+import pytest
 
+
+@pytest.fixture()
+def app(request):
+    fixture = Application_contact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
--- test/test_add_grup.py
+++ test/test_add_grup.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+from fixture.application_group import Application_group
+import pytest
 
+@pytest.fixture()
+def app(request):
+    fixture = Application_group()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 def test_test_add_grup(app):
     app.session.login(username = "admin", password = "secret")
