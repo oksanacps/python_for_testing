9e317a180fef074673d05ec2f4802a42a2405aae
Oksana <Ksundeli76@gmail.com>
2018-02-08 22:24:03 +0300

    edit_contact

---
+++ test/edit_contact.py
@@ -0,0 +1,13 @@
+from model.contact import Contact
+
+def test_edit_contact_from_above(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_contact_from_above(Contact(firstname="Gavrilova", middlename="Oksana", lastname="Nikolaevna", company="CPS",
+                               address="Tver", mobile="89301512526", byear="1993"))
+    app.session.logout()
+
+def test_edit_contact_from_below(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_contact_from_below(Contact(firstname="234", middlename="Oksana", lastname="Nikolaevna", company="CPS",
+                               address="Tver", mobile="89301512526", byear="1993"))
+    app.session.logout()
