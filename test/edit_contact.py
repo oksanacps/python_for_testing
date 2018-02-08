a899ed1d69b9ebd9bfc92390e87f647aed29ebae
Oksana <Ksundeli76@gmail.com>
2018-02-08 22:34:47 +0300

    с небольшим комментарием

--- test/edit_contact.py
+++ test/edit_contact.py
@@ -1,11 +1,13 @@
 from model.contact import Contact
 
+#проверка на кнопку update сверху
 def test_edit_contact_from_above(app):
     app.session.login(username="admin", password="secret")
     app.contact.edit_contact_from_above(Contact(firstname="Gavrilova", middlename="Oksana", lastname="Nikolaevna", company="CPS",
                                address="Tver", mobile="89301512526", byear="1993"))
     app.session.logout()
 
+#проверка на кнопку update снизу
 def test_edit_contact_from_below(app):
     app.session.login(username="admin", password="secret")
     app.contact.edit_contact_from_below(Contact(firstname="234", middlename="Oksana", lastname="Nikolaevna", company="CPS",
