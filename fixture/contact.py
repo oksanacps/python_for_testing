eada92767a9e5a5d83310e956d1c09c3d3103f85
Oksana <Ksundeli76@gmail.com>
2018-02-17 15:05:27 +0300

    оптимизация переходов между страницами (group и home)

--- fixture/contact.py
+++ fixture/contact.py
@@ -105,8 +105,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        # open groups page
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/")):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
