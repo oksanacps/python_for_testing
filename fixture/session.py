3e852ca3fd018cfe9015a5b8f464c06ce29aeae3
Oksana <Ksundeli76@gmail.com>
2018-02-17 14:45:26 +0300

    добавление предусловий в тестах

--- fixture/session.py
+++ fixture/session.py
@@ -29,7 +29,7 @@ class SessionHelper:
 
     def is_logged_in_as(self, username):
         wd = self.app.wd
-        return wd.find_elements_by_xpath("//div/div[1]/form/b").text == "("+username+")"
+        return wd.find_element_by_xpath("//div/div[1]/form/b").text == "("+username+")"
 
     def logout(self):
         wd = self.app.wd
