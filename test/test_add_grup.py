7ec9e12832f83db567dc1ec3613b325f41699196
Oksana <Ksundeli76@gmail.com>
2018-02-25 21:41:55 +0300

    сравнение списков групп

--- test/test_add_grup.py
+++ test/test_add_grup.py
@@ -4,8 +4,13 @@ from model.group import Group
 
 
 def test_test_add_grup(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name ="grup", header ="GRUOGRUP", footer ="111"))
-
+    new_groups = app.group.get_group_list()
+    assert len (old_groups) + 1 == len (new_groups)
 
 def test_test_add_empty_grup(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name ="", header ="", footer =""))
+    new_groups = app.group.get_group_list()
+    assert len (old_groups) + 1 == len (new_groups)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test", header="", footer=""))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name ="123", header ="456", footer ="789"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
