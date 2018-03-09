3277fa1df70e6b609713e82a2c9be4536789b5ad
Oksana <Ksundeli76@gmail.com>
2018-03-09 23:40:17 +0300

    randomness for groups

--- test/test_add_grup.py
+++ test/test_add_grup.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_test_add_grup(app):
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids = [repr(x) for x in testdata])
+def test_test_add_grup(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name ="group", header ="GRUOGRUP", footer ="111")
     app.group.create(group)
     assert len (old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
-
-def test_test_add_empty_grup(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name ="", header ="", footer ="")
-    app.group.create(group)
-    assert len (old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,12 +1,26 @@
 from model.group import Group
 from random import randrange
+import pytest
+import random
+import string
 
-def test_edit_first_group(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation +" "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids = [repr(x) for x in testdata])
+def test_edit_first_group(app, group):
     if app.group.count() == 0:
-        app.group.create(Group(name="test", header="", footer=""))
+        app.group.create(group)
     old_groups = app.group.get_group_list()
     index = randrange(len(old_groups))
-    group = Group(name ="123", header ="456", footer ="789")
     group.id = old_groups[index].id
     app.group.edit_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
