f2b5a3259fd6c69ef6773a21ea93389ac919f76b
Oksana <Ksundeli76@gmail.com>
2018-03-10 16:26:49 +0300

    ???

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,19 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = WebDriver(capabilities={"marionette": False})
+    def __init__(self, browser="firefox"):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper (self)
         self.group = GroupHelper (self)
         self.contact = ContactHelper(self)
