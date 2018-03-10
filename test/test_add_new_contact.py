ddbe4787abd93ed9f4d9788fd0453c10ace70bc9
Oksana <Ksundeli76@gmail.com>
2018-03-10 13:16:36 +0300

    randomness for groups and contacts

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_new_contact(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testinfo = [Contact(firstname="", middlename="", lastname="", company="", address="", mobile="", byear="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10),
+                    company=random_string("company", 3), address=random_string("address", 5), mobile=random_string("mobile", 5), byear="1986")
+            for i in range(4)
+]
+
+@pytest.mark.parametrize("contact", testinfo, ids = [repr(x) for x in testinfo])
+def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Gavrilov", middlename="Nikita", lastname="Romanovich", company="CPS",
-                               address="Tver", mobile="89301512526", byear="1986")
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", middlename="", lastname="", company="",
-                               address="", mobile="", byear="")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,15 +1,28 @@
 from model.contact import Contact
 from random import randrange
+import pytest
+import random
+import string
 
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testinfo = [Contact(firstname="", middlename="", lastname="", company="", address="", mobile="", byear="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10),
+                    company=random_string("company", 3), address=random_string("address", 5), mobile=random_string("mobile", 5), byear="1986")
+            for i in range(4)
+]
+
+@pytest.mark.parametrize("contact", testinfo, ids = [repr(x) for x in testinfo])
 #проверка на кнопку update сверху
-def test_edit_contact_from_above(app):
+def test_edit_contact_from_above(app, contact):
     if app.contact.count()==0:
-        app.contact.create(Contact(firstname="Test", middlename="", lastname="", company="",
-                                   address="", mobile="", byear=""))
+        app.contact.create(contact)
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contact(firstname="Gavrilova", middlename="Oksana", lastname="Nikolaevna", company="CPS",
-                               address="Tver", mobile="89301512526", byear="1993")
     contact.id = old_contacts[index].id
     app.contact.edit_contact_from_above_by_index(contact, index)
     assert len(old_contacts) == app.contact.count()
@@ -17,15 +30,13 @@ def test_edit_contact_from_above(app):
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
+@pytest.mark.parametrize("contact", testinfo, ids = [repr(x) for x in testinfo])
 #проверка на кнопку update снизу
-def test_edit_contact_from_below(app):
+def test_edit_contact_from_below(app, contact):
     if app.contact.count()==0:
-        app.contact.create(Contact(firstname="Test", middlename="", lastname="", company="",
-                                   address="", mobile="", byear=""))
+        app.contact.create(contact)
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
-    contact = Contact(firstname="234", middlename="Oksana", lastname="Nikolaevna", company="CPS",
-                               address="Tver", mobile="89301512526", byear="1993")
     contact.id = old_contacts[index].id
     app.contact.edit_contact_from_below_by_index(contact, index)
     assert len(old_contacts) == app.contact.count()
