f1bceebd4b35ef82d5de1fdb9ae23889434a6ad8
Oksana <Ksundeli76@gmail.com>
2018-03-14 21:45:26 +0300

    чтение из файла

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -19,7 +19,7 @@ testdata = [Group(name="", header="", footer="")] + [
 def test_test_add_grup(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
-    assert len (old_groups) + 1 == app.group.count()
+    assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key = Group.id_or_max) == sorted(new_groups, key = Group.id_or_max)
