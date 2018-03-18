cb44b307875c48c9c01d5cc69f327fc3a88bef75
Oksana <Ksundeli76@gmail.com>
2018-03-18 23:14:29 +0300

    генератор тестовых данных + библиотека jsonpickle

--- generator/contact.py
+++ generator/contact.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -14,8 +14,8 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 
-n=5
-f="data/contact.json"
+n = 5
+f = "data/contact.json"
 
 for o, a in opts:
     if o == "-n":
@@ -38,4 +38,5 @@ testinfo = [Contact(firstname="", middlename="", lastname="", company="", addres
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testinfo, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testinfo))
