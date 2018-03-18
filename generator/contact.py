2438b0a618bf57e0458f72a577454223a5022e78
Oksana <Ksundeli76@gmail.com>
2018-03-18 21:19:33 +0300

    генератор тестовых данных

---
+++ generator/contact.py
@@ -0,0 +1,41 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+n=5
+f="data/contact.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testinfo = [Contact(firstname="", middlename="", lastname="", company="", address="", mobile="", byear="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10),
+                    company=random_string("company", 3), address=random_string("address", 5), mobile=random_string("mobile", 5), byear="1986")
+            for i in range(n)
+]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testinfo, default=lambda x: x.__dict__, indent=2))
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -1,20 +1,7 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 import pytest
-import random
-import string
-
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testinfo = [Contact(firstname="", middlename="", lastname="", company="", address="", mobile="", byear="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10),
-                    company=random_string("company", 3), address=random_string("address", 5), mobile=random_string("mobile", 5), byear="1986")
-            for i in range(4)
-]
+from data.add_contact import constant as testinfo
 
 @pytest.mark.parametrize("contact", testinfo, ids = [repr(x) for x in testinfo])
 def test_add_new_contact(app, contact):
