e4aef28c0a5eff33acc7095f9995f3d49637d608
Oksana <Ksundeli76@gmail.com>
2018-03-25 13:18:01 +0300

    чтение из БД с поправками

--- fixture/group.py
+++ fixture/group.py
@@ -41,6 +41,15 @@ class GroupHelper:
         self.return_to_group_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+        self.group_cache = None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -48,6 +57,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def edit_first_group(self, group):
         self.edit_group_by_index(group, 0)
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,27 +1,14 @@
-from model.group import Group
-from random import randrange
-import pytest
 import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-]
-
-@pytest.mark.parametrize("group", testdata, ids = [repr(x) for x in testdata])
-def test_delete_some_group(app, group):
-    if app.group.count() == 0:
+def test_delete_some_group(app, db, json_groups):
+    group = json_groups
+    if len(db.get_group_list()) == 0:
         app.group.create(group)
-    old_groups = app.group.get_group_list()
-    index = randrange (len(old_groups))
-    app.group.delete_group_by_index(index)
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
     assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []
+    new_groups = db.get_group_list()
+    old_groups.remove(group)
     assert old_groups == new_groups
