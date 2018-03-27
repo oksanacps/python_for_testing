6b358e1900518c02ea0732d95fff2cedb24272e1
Oksana <Ksundeli76@gmail.com>
2018-03-27 23:46:49 +0300

    сравнение всех контактов с данными из БД

--- fixture/db.py
+++ fixture/db.py
@@ -27,10 +27,11 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, mobile, work, fax, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, homephone, mobilephone, workphone, secondaryphone, email_0, email_1, email_2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, homephone=homephone, mobilephone=mobilephone,
+                                    workphone=workphone, secondaryphone=secondaryphone, email_0=email_0, email_1=email_1, email_2=email_2))
         finally:
             cursor.close()
         return list
--- test/test_properties.py
+++ test/test_properties.py
@@ -2,16 +2,30 @@ import re
 from random import randrange
 
 
-def test_phones_on_home_page(app):
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
-    assert clear(contact_from_home_page.firstname) == clear(contact_from_edit_page.firstname)
-    assert clear(contact_from_home_page.lastname) == clear(contact_from_edit_page.lastname)
-    assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
+def test_all_contact_ui_db(app, db):
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    for contact_home in contact_from_home_page:
+        for contact_db in contact_from_db:
+            if contact_home.id == contact_db.id:
+                assert contact_home.firstname == contact_db.firstname
+                assert contact_home.lastname == contact_db.lastname
+                assert contact_home.address == contact_db.address
+                assert contact_home.all_phones_from_home_page == merge_phones_like_on_home_page(contact_db)
+                assert contact_home.all_email_from_home_page == merge_email_like_on_home_page(contact_db)
+                return
+
+
+# def test_phones_on_home_page(app):
+#     old_contacts = app.contact.get_contact_list()
+#     index = randrange(len(old_contacts))
+#     contact_from_home_page = app.contact.get_contact_list()[index]
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+#     assert contact_from_home_page.all_email_from_home_page == merge_email_like_on_home_page(contact_from_edit_page)
+#     assert clear(contact_from_home_page.firstname) == clear(contact_from_edit_page.firstname)
+#     assert clear(contact_from_home_page.lastname) == clear(contact_from_edit_page.lastname)
+#     assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
 
 
 # def test_phones_contact_view_page(app):
